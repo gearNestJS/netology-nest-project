241c8d45f22ccb74c721dbb07d0b1fc096044ea5
Verda Muso <gearmobile@gmail.com>
2024-02-19 00:58:22 +0300

    add validation

--- src/books/dto/add-book.dto.ts
+++ src/books/dto/add-book.dto.ts
@@ -1,7 +1,16 @@
 import { IBook } from '../types';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class AddBookDto implements IBook {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsInt()
+  @IsNotEmpty()
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   slug: string;
 }
